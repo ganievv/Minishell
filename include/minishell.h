c007244d4fa86c9f507204ecac6ecf943b4ce331
Thomas Nakas <tnakas@2-B-4.42heilbronn.de>
2024-07-22 21:05:14 +0200

    test function

--- include/minishell.h
+++ include/minishell.h
@@ -6,7 +6,7 @@
 /*   By: tnakas <tnakas@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/15 15:56:18 by tnakas            #+#    #+#             */
-/*   Updated: 2024/07/22 21:03:22 by tnakas           ###   ########.fr       */
+/*   Updated: 2024/07/22 21:04:57 by tnakas           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -110,5 +110,6 @@ int			count_args(char **args);
 int			is_nbr(char *arg);
 int			is_valid_exit_range(char *nbr);
 long long	ft_atoll(char *str);
+long long	test;
 //protected
 #endif
