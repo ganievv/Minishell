2b02c941cc9e415589eb6858c14a0fbf924d5740
Semen Ganiev <sganiev@1-B-28.42heilbronn.de>
2024-07-25 16:12:39 +0200

    created export builtin

--- src/executor/builtins_2.c
+++ src/executor/builtins_2.c
@@ -6,7 +6,7 @@
 /*   By: sganiev <sganiev@student.42heilbronn.de    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/22 22:48:14 by sganiev           #+#    #+#             */
-/*   Updated: 2024/07/25 15:03:00 by sganiev          ###   ########.fr       */
+/*   Updated: 2024/07/25 16:04:24 by sganiev          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,29 +15,38 @@
 /* I am assuming that we have skipped all flags
 *  in lexer or parser step*/
 
-int	ft_export(char **args, char **envp)
+int	ft_export(char **args, t_env_vars **env_vars)
 {
-	int	i;
+	t_env_vars *curr;
 
-	i = 0;
-	if (!args[i])
+	curr = *env_vars;
+	if (!*args)
 	{
+		while (curr)
+		{
+			write (STDOUT_FILENO, "declare -x ", 11);
+			write (STDOUT_FILENO, curr->var, ft_strlen(curr->var));
+			write (STDOUT_FILENO, "\n", 1);
+			curr = curr->next;
+		}
 	}
 	else
 	{
-		while (args[i])
+		while (*args)
 		{
-			if (!is_export_arg_valid(args[i]))
+			if (!is_export_arg_valid(*args))
 				return (1);
-			i++;
+			create_node(*args, env_vars);
+			args++;
 		}
 	}
+	return (0);
 }
 
-int	ft_unset(char **args, char **envp)
+int	ft_unset(char **args, t_env_vars **env_vars)
 {
 }
 
-int	ft_env(char **args, char **envp)
+int	ft_env(char **args, t_env_vars **env_vars)
 {
 }
