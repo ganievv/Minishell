bd455888a5ac3938563a238cb13a672d2f83df98
Semen Ganiev <sganiev@2-B-2.42heilbronn.de>
2024-07-26 19:31:29 +0200

    created absolute path to executable; started launch of cmd

--- src/executor/executor.c
+++ src/executor/executor.c
@@ -6,49 +6,63 @@
 /*   By: sganiev <sganiev@student.42heilbronn.de    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/15 15:59:44 by tnakas            #+#    #+#             */
-/*   Updated: 2024/07/26 18:30:37 by sganiev          ###   ########.fr       */
+/*   Updated: 2024/07/26 19:01:44 by sganiev          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../include/minishell.h"
+
+static void	exec_cmd(char *absolute_path, char **args, t_env_vars *envp)
+{
+	char	**envp_arr;
+	char	**argv;
+
+	envp_arr = linked_list_to_arr(envp);
+	argv = args_to_argv(args);
+	execve(absolute_path, argv, envp_arr);
+}
 
 /* this function executes an external command in a new process;
 *  it first searches for the command's executable in the 'PATH'
 *  environment variable; if the executable is not found there,
 *  the function assumes the command has an absolute path and
 *  executes it directly										 */
-static int	exec_external_cmd(char *cmd, t_msh *info)
+static int	launch_external_cmd(char *cmd, t_msh *info)
 {
 	char	*path_env_v;
 	char	*dir_of_exec;
+	char	*absolute_path;
 
 	dir_of_exec = NULL;
 	path_env_v = search_env_var(info->env_vars, "PATH");
 	if (path_env_v)
 		dir_of_exec = search_exec_dir(cmd, (ft_strchr(path_env_v, '=') + 1));
-	if (!dir_of_exec)
-		dir_of_exec = cmd;
-	int	id = fork();
-	if (id == 0)
+	if (dir_of_exec)
 	{
-		return (0);
+		absolute_path = ft_strjoin(dir_of_exec, cmd);
+		free(dir_of_exec);
 	}
+	else
+		absolute_path = ft_strdup(cmd);
+	exec_cmd(absolute_path, info->cmds->args, info->env_vars);
+	if (absolute_path)
+		free(absolute_path);
 }
 
 /* this function executes one command;
 *  it does not create a new process if
 *  this command is a builtin		*/
-static int	exec_one_cmd(t_msh *info)
+static int	launch_one_cmd(t_msh *info)
 {
 	int	index;
 
 	if (info->cmds->file)
-		make_redirection();
+		make_redirection(info->cmds);
 	index = is_cmd_builtin(info->cmds->command, info);
 	if (index >= 0)
 		(info->builtin_ptrs[index])(info->cmds->args, &info->env_vars);
 	else
-		exec_external_cmd(info->cmds->command, info);
+		launch_external_cmd(info->cmds->command, info);
 }
 
 /* this function launches all commands from
@@ -59,7 +73,7 @@ int	exec_all_cmds(t_msh *info)
 	init_builtin_ptrs(info->builtin_ptrs);
 	init_env_vars_list(info);
 	if (count_cmds(info->cmds) == 1)
-		exec_one_cmd(info);
+		launch_one_cmd(info);
 	else
 	{
 		while (info->cmds != NULL)
