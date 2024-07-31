7649bbb3b2f4316f08615642669bf1aa4c57ca57
Semen Ganiev <sganiev@2-D-1.42heilbronn.de>
2024-07-31 19:32:59 +0200

    added new comment with explanation

--- src/executor/u_wait_for_proc.c
+++ src/executor/u_wait_for_proc.c
@@ -1,18 +1,21 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   u_wait_processes.c                                 :+:      :+:    :+:   */
+/*   u_wait_for_proc.c                                  :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: sganiev <sganiev@student.42heilbronn.de    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/31 18:41:25 by sganiev           #+#    #+#             */
-/*   Updated: 2024/07/31 19:15:36 by sganiev          ###   ########.fr       */
+/*   Updated: 2024/07/31 19:32:29 by sganiev          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../include/minishell.h"
 
-/* this function waits for all command to finish their execution*/
+/* this function waits for all commands to finish
+*  their execution; if it is the last command to
+*  execute, it will save its exit status in the 
+*  info->last_exit_status */
 void	wait_for_processes(t_msh *info, int cmds_num)
 {
 	int	i;
