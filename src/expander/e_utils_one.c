360405ca43f5304d09f3961938f46a470605f2d2
Thomas Nakas <tnakas@2-E-9.42heilbronn.de>
2024-08-07 17:25:24 +0200

    adding the NULL protections

--- src/expander/e_utils_one.c
+++ src/expander/e_utils_one.c
@@ -6,7 +6,7 @@
 /*   By: tnakas <tnakas@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/08/04 18:49:43 by tnakas            #+#    #+#             */
-/*   Updated: 2024/08/06 20:47:11 by tnakas           ###   ########.fr       */
+/*   Updated: 2024/08/07 17:24:18 by tnakas           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,9 +18,13 @@ static void	expand_double_quoted_helper_one(int l, t_exp_helper *h, char **envp)
 	while (ft_isalnum(*(h->end)) || *(h->end) == '_' || *(h->end) == '?')
 		(h->end)++;
 	h->var = ft_strndup((h->start), (h->end) - (h->start));
+	if (!h->var)
+		return ;
 	h->temp = h->result;
 	h->result = (char *)malloc(ft_strlen(h->result)
 			+ ft_strlen(expand_var(l, h->var, envp)) + 1);
+	if (!h->result)
+		return ;
 	ft_strcpy(h->result, h->temp);
 	ft_strcpy(h->result + ft_strlen(h->temp), expand_var(l, h->var, envp));
 	free(h->temp);
@@ -28,27 +32,42 @@ static void	expand_double_quoted_helper_one(int l, t_exp_helper *h, char **envp)
 	(h->start) = (h->end);
 }
 
+static void	expand_double_quoted_helper_two(t_exp_helper *h)
+{
+	h->temp = h->result;
+	h->len = ft_strlen(h->result) + 2;
+	h->result = (char *)malloc(h->len);
+	if (!h->result)
+		return ;
+	ft_strcpy(h->result, h->temp);
+	h->result[h->len - 2] = *(h->start);
+	h->result[h->len - 1] = '\0';
+	free(h->temp);
+	(h->start)++;
+}
+
 char	*expand_double_quoted(int l, char *input, char **envp)
 {
 	t_exp_helper	h;
+	char			*temp;
 
 	h.result = ft_strdup("");
+	if (!h.result)
+		return (NULL);
 	(h.start) = input;
 	while (*(h.start))
 	{
 		if (*(h.start) == '$')
 			expand_double_quoted_helper_one(l, &h, envp);
 		else
-		{
-			h.temp = h.result;
-			h.len = ft_strlen(h.result) + 2;
-			h.result = (char *)malloc(h.len);
-			ft_strcpy(h.result, h.temp);
-			h.result[h.len - 2] = *(h.start);
-			h.result[h.len - 1] = '\0';
-			free(h.temp);
-			(h.start)++;
-		}
+			expand_double_quoted_helper_two(&h);
 	}
-	return (ft_strtrim(h.result, "\""));
+	temp = ft_strtrim(h.result, "\"");
+	if (!temp)
+	{
+		free(h.result);
+		return (NULL);
+	}
+	free(h.result);
+	return (temp);
 }
