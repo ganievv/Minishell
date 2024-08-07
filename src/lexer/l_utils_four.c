946c84e4aefff62d53258ac1183042c64b22cb47
Thomas Nakas <tnakas@2-E-9.42heilbronn.de>
2024-08-07 16:19:03 +0200

    correcting the error checking functions

--- src/lexer/l_utils_four.c
+++ src/lexer/l_utils_four.c
@@ -6,7 +6,7 @@
 /*   By: tnakas <tnakas@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/08/06 21:42:25 by tnakas            #+#    #+#             */
-/*   Updated: 2024/08/06 21:55:14 by tnakas           ###   ########.fr       */
+/*   Updated: 2024/08/07 16:16:23 by tnakas           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,15 +14,17 @@
 
 static int	pipe_after_pipe(t_token *head)
 {
-	return (head->type == PIPE && head->next && head->next->type == PIPE);
+	printf("head->type == pipe || %d\n", head->type == 2);
+	printf("head->next->type == pipe || %d\n", head->next->type == 2);
+	return (head->type == 2 && (!(head->next) || head->next->type == 2));
 }
 
 static int	redir_and_after_not_word(t_token *head)
 {
-	return ((head->type == REDIR_IN || head->type == REDIR_OUT
-			|| head->type == APPEND_DELIMITER
-			|| head->type == DELIMITER || head->type == PIPE)
-		&& (head->next || head->next->type != WORD));
+	return ((head->type == 4 || head->type == 3
+			|| head->type == 6
+			|| head->type == 5 || head->type == 2)
+		&& (head->next || head->next->type != 0));
 }
 
 void	check_syntax_errors(t_token **head)
@@ -32,13 +34,14 @@ void	check_syntax_errors(t_token **head)
 	current = *head;
 	while (current)
 	{
-		if (pipe_after_pipe(*head))
+		printf("type : %d\n", current->type);
+		if (pipe_after_pipe(current))
 		{
 			ft_putstr_fd("Syntax error:  consecutive pipes\n", 2);
 			token_free(head);
 			exit(1);
 		}
-		if (redir_and_after_not_word(*head))
+		if (redir_and_after_not_word(current))
 		{
 			ft_putstr_fd("Syntax error:  invalid redirections\n", 2);
 			token_free(head);
