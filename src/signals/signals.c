5bd478954d32dfdeee69e96401e7f858557b4c90
Semen Ganiev <sganiev@2-B-2.42heilbronn.de>
2024-08-10 17:10:31 +0200

    added ignoring of SIGQUIT

--- src/signals/signals.c
+++ src/signals/signals.c
@@ -6,7 +6,7 @@
 /*   By: sganiev <sganiev@student.42heilbronn.de    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/15 15:59:34 by tnakas            #+#    #+#             */
-/*   Updated: 2024/08/10 17:04:08 by sganiev          ###   ########.fr       */
+/*   Updated: 2024/08/10 17:08:54 by sganiev          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,10 +23,5 @@ void	handle_signal(int signal)
 		rl_on_new_line();
 		rl_replace_line("", 0);
 		rl_redisplay();
-	}
-	else if (signal == SIGQUIT)
-	{
-		write(STDOUT_FILENO, "Quit: 3\n", 8);
-		kill(0, SIGCHLD);
 	}
 }
