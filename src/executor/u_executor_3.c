953be1f5d6324517e429053824ab01d34d8e39af
Semen Ganiev <sganiev@2-C-6.42heilbronn.de>
2024-08-12 18:31:45 +0200

    moved save_io_fds() and restore_io_fds() to u_files_redir.c

--- src/executor/u_executor_3.c
+++ src/executor/u_executor_3.c
@@ -6,7 +6,7 @@
 /*   By: sganiev <sganiev@student.42heilbronn.de    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/31 19:49:23 by sganiev           #+#    #+#             */
-/*   Updated: 2024/08/12 18:12:34 by sganiev          ###   ########.fr       */
+/*   Updated: 2024/08/12 18:29:37 by sganiev          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,37 +22,6 @@ char	*str_to_lower_case(const char *cmd)
 	while (cmd_cp && cmd_cp[++i])
 		cmd_cp[i] = ft_tolower(cmd_cp[i]);
 	return (cmd_cp);
-}
-
-int	*save_io_fds(t_pipe_group *cmd)
-{
-	int	*fds;
-
-	fds = (int *)malloc(sizeof(int) * 2);
-	if (!fds)
-		return (NULL);
-	if (cmd->file_in || cmd->is_heredoc_in)
-		fds[0] = dup(cmd->redir_in);
-	if (cmd->file_out)
-		fds[1] = dup(cmd->redir_out);
-	return (fds);
-}
-
-void	restore_io_fds(int *fds, t_pipe_group *cmd)
-{
-	if (!fds)
-		return ;
-	if (cmd->file_in || cmd->is_heredoc_in)
-	{
-		dup2(fds[0], cmd->redir_in);
-		close(fds[0]);
-	}
-	if (cmd->file_out)
-	{
-		dup2(fds[1], cmd->redir_out);
-		close(fds[1]);
-	}
-	free(fds);
 }
 
 void	print_cmd_not_found(char *cmd)
--- src/executor/u_files_redir.c
+++ src/executor/u_files_redir.c
@@ -6,7 +6,7 @@
 /*   By: sganiev <sganiev@student.42heilbronn.de    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/08/12 17:54:08 by sganiev           #+#    #+#             */
-/*   Updated: 2024/08/12 18:17:58 by sganiev          ###   ########.fr       */
+/*   Updated: 2024/08/12 18:30:20 by sganiev          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -30,7 +30,7 @@ int	make_files_redir(t_pipe_group *cmd)
 	{
 		fd = open(cmd->file_in, cmd->mode_in);
 		if (fd == -1)
-			return (perror("msh"), 0);
+			return (perror(cmd->file_in), 0);
 		dup2(fd, cmd->redir_in);
 		close(fd);
 	}
@@ -38,9 +38,40 @@ int	make_files_redir(t_pipe_group *cmd)
 	{
 		fd = open(cmd->file_out, cmd->mode_out, 0666);
 		if (fd == -1)
-			return (perror("msh"), 0);
+			return (perror(cmd->file_out), 0);
 		dup2(fd, cmd->redir_out);
 		close(fd);
 	}
 	return (1);
+}
+
+int	*save_io_fds(t_pipe_group *cmd)
+{
+	int	*fds;
+
+	fds = (int *)malloc(sizeof(int) * 2);
+	if (!fds)
+		return (NULL);
+	if (cmd->file_in || cmd->is_heredoc_in)
+		fds[0] = dup(cmd->redir_in);
+	if (cmd->file_out)
+		fds[1] = dup(cmd->redir_out);
+	return (fds);
+}
+
+void	restore_io_fds(int *fds, t_pipe_group *cmd)
+{
+	if (!fds)
+		return ;
+	if (cmd->file_in || cmd->is_heredoc_in)
+	{
+		dup2(fds[0], cmd->redir_in);
+		close(fds[0]);
+	}
+	if (cmd->file_out)
+	{
+		dup2(fds[1], cmd->redir_out);
+		close(fds[1]);
+	}
+	free(fds);
 }
