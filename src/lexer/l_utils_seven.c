e4956df48867998ec9a267544ab3de675dff758b
Thomas Nakas <tnakas@2-E-4.42heilbronn.de>
2024-08-17 20:35:36 +0200

    taking out the testing prints

--- src/lexer/l_utils_seven.c
+++ src/lexer/l_utils_seven.c
@@ -6,7 +6,7 @@
 /*   By: tnakas <tnakas@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/08/13 22:05:41 by tnakas            #+#    #+#             */
-/*   Updated: 2024/08/17 20:27:29 by tnakas           ###   ########.fr       */
+/*   Updated: 2024/08/17 20:34:57 by tnakas           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -74,11 +74,8 @@ void	token_preexp_to_token_exp(int l, t_token **dest, char **envp)
 
 	temp = *dest;
 	prev = NULL;
-	printf("//========================\n");
 	while (temp)
 	{
-		printf("inside of the loop \n");
-		printf("toke_start before = %s\n", temp->token_start);
 		if ((temp->type == EXP_FIELD || ((temp->type == D_QUOTED)
 					&& ft_strchr(temp->token_start, '$')))
 			&& (!prev || prev->type != HEREDOC))
@@ -94,11 +91,8 @@ void	token_preexp_to_token_exp(int l, t_token **dest, char **envp)
 		}
 		if (temp->type != SPC)
 			prev = temp;
-		printf("toke_start before = %s\n", temp->token_start);
 		temp = temp->next;
-		printf("inside of the loop \n");
 	}
-	printf("//========================\n");
 }
 
 void	token_ready_for_parsing(int l, t_token *src, t_token **dest,
@@ -107,5 +101,4 @@ void	token_ready_for_parsing(int l, t_token *src, t_token **dest,
 	token_to_token_preexp(src, dest);
 	token_preexp_to_trimed(dest);
 	token_preexp_to_token_exp(l, dest, envp);
-	print_tokens(*dest);
 }
